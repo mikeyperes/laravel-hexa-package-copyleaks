771d596a6d64e46d3a9f7f7432da100301d4cdd6
mikeyperes <michael@mike-ro-tech.com>
2026-04-09 03:48:54 -0400

    Version bump

--- config/copyleaks.php
+++ config/copyleaks.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'enabled' => true,
     'login_url' => 'https://id.copyleaks.com/v3/account/login/api',
     'detect_url' => 'https://api.copyleaks.com/v2/writer-detector/{scanId}/check',
